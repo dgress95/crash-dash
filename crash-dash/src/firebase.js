ca43eea8d6fe9bdbd8dd64856d9d6a9c225dca55
Diane Gress <diane.gress@criticalmass.com>
2024-07-27 12:21:57 -0500

    Sets up Firebase config

---
+++ crash-dash/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBXVSfwe6QMR651DO3BkPNy51KzIsZntE",
+  authDomain: "crash-dash.firebaseapp.com",
+  databaseURL: "https://crash-dash-default-rtdb.firebaseio.com",
+  projectId: "crash-dash",
+  storageBucket: "crash-dash.appspot.com",
+  messagingSenderId: "881990990447",
+  appId: "1:881990990447:web:8413981ae7d5267668ab9a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
